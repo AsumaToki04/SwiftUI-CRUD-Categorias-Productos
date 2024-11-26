2f12038094eddc09f1c94e71209c400344ba7b3b
AsumaToki04 <megamanzxu@hotmail.com>
2024-11-26 12:18:14 -0800

    Initial Commit

---
+++ SwiftUI-CRUD-Categorias-Productos/SwiftUI_CRUD_Categorias_ProductosApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_CRUD_Categorias_ProductosApp.swift
+//  SwiftUI-CRUD-Categorias-Productos
+//
+//  Created by Asuma Toki on 11/26/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_CRUD_Categorias_ProductosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
